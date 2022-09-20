c28dc0889294785cb9f50d2df82156debb879428
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-09-20 23:59:05 +0200

    Add auth form

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -8,6 +8,33 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _signupMode = false;
+
+  String _email = '';
+  String _username = '';
+  String _password = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState?.validate() == true;
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState?.save();
+
+      print(_email);
+      print(_username);
+      print(_password);
+    }
+  }
+
+  void _toggleSignupMode() {
+    setState(() {
+      _signupMode = !_signupMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,34 +44,62 @@ class _AuthFormState extends State<AuthForm> {
             child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
+            key: _formKey,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextFormField(
+                  key: const ValueKey("email"),
+                  validator: (value) =>
+                      value == null || value.isEmpty || !value.contains('@')
+                          ? 'Please enter a valid email address'
+                          : null,
                   keyboardType: TextInputType.emailAddress,
                   decoration: const InputDecoration(
                     labelText: "Email address",
                   ),
+                  onSaved: (value) {
+                    _email = value ?? '';
+                  },
                 ),
-                TextFormField(
+                if (_signupMode)
+                  TextFormField(
+                    key: const ValueKey("username"),
+                    validator: (value) =>
+                        _signupMode && (value == null || value.length < 4)
+                            ? 'A username should be at least 4 characters long'
+                            : null,
                     decoration: const InputDecoration(
-                  labelText: "Username",
-                )),
+                      labelText: "Username",
+                    ),
+                    onSaved: (value) {
+                      _username = value ?? "";
+                    },
+                  ),
                 TextFormField(
+                  key: const ValueKey("password"),
+                  validator: (value) => value == null || value.length < 8
+                      ? 'A password should be at least 8 characters long'
+                      : null,
                   decoration: const InputDecoration(
                     labelText: "Password",
                   ),
                   obscureText: true,
+                  onSaved: (value) {
+                    _password = value ?? "";
+                  },
                 ),
                 const SizedBox(
                   height: 12,
                 ),
                 ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Login")),
+                    onPressed: _trySubmit,
+                    child: Text(_signupMode ? "Sign up" : "Login")),
                 TextButton(
-                  onPressed: () {},
-                  child: const Text("Create a new account"),
+                  onPressed: _toggleSignupMode,
+                  child: Text(_signupMode
+                      ? "I already have an account"
+                      : "Create a new account"),
                 )
               ],
             ),
