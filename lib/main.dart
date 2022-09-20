eec333c06f6504c3db1276d503e226fc0bfa3090
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-09-20 23:23:03 +0200

    Add other files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/screens/chat_screen.dart';
+import 'package:flutter_chat/firebase_options.dart';
+import 'package:flutter_chat/screens/auth_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ChatScreen()
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        return snapshot.connectionState == ConnectionState.done
+            ? MaterialApp(
+                title: 'Flutter Chat',
+                theme: ThemeData(
+                    primarySwatch: Colors.pink,
+                    backgroundColor: Colors.pink,
+                    colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+                    elevatedButtonTheme: ElevatedButtonThemeData(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.pink,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)))),
+                    buttonTheme: ButtonTheme.of(context).copyWith(
+                        buttonColor: Colors.pink,
+                        textTheme: ButtonTextTheme.primary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)))),
+                home: const AuthScreen(),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              );
+      },
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,15 +7,35 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter Chat"),
-        ),
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (ctx, index) {
-              return Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text("It works"));
-            }));
+      appBar: AppBar(
+        title: const Text("Flutter Chat"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chats/Agodhvj7GD75OwNwWSfb/messages")
+            .snapshots(),
+        builder: (context, snapshot) {
+          return snapshot.connectionState == ConnectionState.waiting
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (ctx, index) {
+                    return Container(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(snapshot.data?.docs[index]['text'] ?? ""));
+                  });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection("chats/Agodhvj7GD75OwNwWSfb/messages")
+              .add({
+            'text' : "It is time to chew bubble gum and kick ass. And I'm all out of bubble gum."
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
